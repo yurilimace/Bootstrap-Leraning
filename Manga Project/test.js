af468ac1f10c9ba493fe2da7790a4962f1b9d1a3
José Yuri <yurilima.ce@gmail.com>
2018-09-09 14:39:49 -0300

    adding a javascript script to check login and password to go to the main page

--- Manga Project/test.js
+++ Manga Project/test.js
@@ -2,9 +2,10 @@
 function check_login(){
     var login = document.getElementById('email').value;
     var password = document.getElementById('senha').value;
-
+    var link = document.getElementById("teste").href;
     if( login == "test@gmail.com" && password == "1234"){
-        alert("Success");
+        alert("success");
+        document.getElementById('teste').href = "main.html";
     }
     else{
         alert("Wrong");
